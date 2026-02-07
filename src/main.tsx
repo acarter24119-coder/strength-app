319ae957d383c211b286ff2f6553a2391a1095c2
Aaron <acarter24119@email.com>
2026-02-07 22:06:44 +0000

    Fix MainPage casing issue and restore CSS theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MainPage from "./Mainpage";
-import "./index.css";
 import "./App.css";
+import "./index.css";
+import MainPage from "./Mainpage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
